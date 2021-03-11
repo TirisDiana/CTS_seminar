3868d54187c6c8e54e462b92c572733fa92eab61
TirisDiana <tirisdiana18@stud.ase.ro>
2021-03-11 12:35:03 +0200

    s3

--- seminar3/src/ro/ase/csie/cts/g1088/laborator3/faza1/Produs.java
+++ seminar3/src/ro/ase/csie/cts/g1088/laborator3/faza1/Produs.java
@@ -1,26 +1,33 @@
 package ro.ase.csie.cts.g1088.laborator3.faza1;
 
 public class Produs {
-	public float getPretFinal(int tipProdus, float pretInitial, int vechimeClientInAni)
+	//final=constante
+	public static final int VECHIME_CLIENT_MAXIMA=10;
+	public static final float DISCOUNT_CLIENT_MAXIM=0.15f;
+	
+	public float getPretFinal(TipProdus tipProdus, float pretInitial, int vechimeClientInAni)
 	  {
 	    float pretFinal = 0;
-	    float discountFidelitate = (vechimeClientInAni > 10) ? (float)15/100 : (float)vechimeClientInAni/100; 
-	    if (tipProdus == 1)
+	    float discountFidelitate = (vechimeClientInAni > VECHIME_CLIENT_MAXIMA) ? DISCOUNT_CLIENT_MAXIM  : (float)vechimeClientInAni/100; 
+	    if (tipProdus == TipProdus.NOU)
 	    {
 	      pretFinal = pretInitial;
 	    }
-	    else if (tipProdus == 2)
+	    else if (tipProdus == TipProdus.DISCOUNT)
 	    {
 	      pretFinal = (pretInitial - (0.1f * pretInitial)) - discountFidelitate * (pretInitial - (0.1f * pretInitial));
 	    }
-	    else if (tipProdus == 3)
+	    else if (tipProdus == TipProdus.STOC_LIMITAT)
 	    {
 	      pretFinal = (pretInitial - (0.25f * pretInitial)) - discountFidelitate * (pretInitial - (0.25f * pretInitial));
 	    }
-	    else if (tipProdus == 4)
+	    else if (tipProdus == TipProdus.VECHI)
 	    {
 	      pretFinal = (pretInitial - (0.35f * pretInitial)) - discountFidelitate * (pretInitial - (0.35f * pretInitial));
 	    }
 	    return pretFinal;
 	  }
 }
+
+//cand avem nev de enumerari: am un set finit de valori posibile pt aceeasi variabila
+//la enumerari nu mai e nevoie de validarea datelor
