76ca53b16be8f7177ab43f711c9e81f530f3dc52
TirisDiana <tirisdiana18@stud.ase.ro>
2021-03-18 12:23:38 +0200

    inainte de pauza

---
+++ seminar4/src/ro/ase/csie/cts/g1088/laborator3/faza3/Produs.java
@@ -0,0 +1,38 @@
+package ro.ase.csie.cts.g1088.laborator3.faza3;
+
+import ro.ase.csie.cts.g1088.laborator3.exceptii.ExceptiePretInvalid;
+import ro.ase.csie.cts.g1088.laborator3.exceptii.ExceptieVechimeClient;
+import ro.ase.csie.cts.g1088.laborator3.faza3.servicii.InterfataMarketing;
+import ro.ase.csie.cts.g1088.laborator3.faza3.servicii.InterfataValidare;
+
+public class Produs {
+	
+	InterfataMarketing serviciuMk=null;
+	InterfataValidare serviciuValidare=null;
+	
+	public static float getPretCuDiscount(float pretInitial, float discount) {
+		return pretInitial - (discount * pretInitial);
+	}
+	
+	public float getPretFinal(TipProdus tipProdus, float pretInitial, int vechimeClientInAni) 
+			throws ExceptiePretInvalid, ExceptieVechimeClient
+	  {
+		
+		serviciuValidare.validarePret(pretInitial);
+		serviciuValidare.validareVechimeClient(vechimeClientInAni);
+		
+	    
+	    float discountFidelitate=(tipProdus==TipProdus.NOU)? 0: serviciuMk.getDiscountFidelitate(vechimeClientInAni);	   
+	    
+	  	float discount = tipProdus.getDiscount();
+    	float valoareDiscountTipProdus = getPretCuDiscount(pretInitial,discount);
+    	float pretFinal = valoareDiscountTipProdus *(1 - discountFidelitate);
+	    
+	   
+	    
+	    return pretFinal;
+	  }
+}
+
+//cand avem nev de enumerari: am un set finit de valori posibile pt aceeasi variabila
+//la enumerari nu mai e nevoie de validarea datelor
