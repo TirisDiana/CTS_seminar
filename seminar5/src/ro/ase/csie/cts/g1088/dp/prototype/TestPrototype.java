def5a9a7782bc9984e81bd768c07224cbec86ce6
TirisDiana <tirisdiana18@stud.ase.ro>
2021-03-25 13:27:09 +0200

    final

---
+++ seminar5/src/ro/ase/csie/cts/g1088/dp/prototype/TestPrototype.java
@@ -0,0 +1,13 @@
+package ro.ase.csie.cts.g1088.dp.prototype;
+
+public class TestPrototype {
+
+	public static void main(String[] args) {
+		Caracter3D caracter1 = new Caracter3D("model1.3d");
+		//Caracter3D caracter2 = new Caracter3D("model1.3d");
+		Caracter3D caracter3 = new Caracter3D("model1.3d");
+
+		Caracter3D caracter2 = (Caracter3D) caracter1.clone();
+	}
+
+}
